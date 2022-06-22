c0a7a5e231936cffc333457c25c8e14d8c44c779
Matias Pedelhez <matiaspedelhezinf@gmail.com>
2022-06-22 15:31:07 -0300

    next.config changes

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["tailwindui.com", "img.icons8.com", "cdn.shopify.com"],
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_ACCESS_TOKEN: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+    domains: ['tailwindui.com', 'img.icons8.com', 'cdn.shopify.com'],
   },
 };
 
