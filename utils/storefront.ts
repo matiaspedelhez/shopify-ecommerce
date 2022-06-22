287c1078da5fcfcd4a9eed298953c87f5a8f93d9
Matias Pedelhez <matiaspedelhezinf@gmail.com>
2022-06-22 02:19:57 -0300

    unmarked errors from typescript compiler

--- utils/storefront.ts
+++ utils/storefront.ts
@@ -1,9 +1,11 @@
+// @ts-nocheck
+
 export async function storefront(query: string, variables = {}) {
   const response = await fetch(process.env.NEXT_PUBLIC_API_URL, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      "X-Shopify-Storefront-Access-Token": process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+      'Content-Type': 'application/json',
+      'X-Shopify-Storefront-Access-Token': process.env.NEXT_PUBLIC_ACCESS_TOKEN,
     },
     body: JSON.stringify({ query, variables }),
   });
